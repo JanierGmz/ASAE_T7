8854ebf1c86e5663ea05130c48a65134f2eaf8c2
Janier Gomez <jygomezg211@gmail.com>
2025-10-15 09:06:32 -0500

    First Commit

---
+++ src/main/java/co/edu/unicauca/asae_t7/AsaeT7Application.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.asae_t7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsaeT7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsaeT7Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unicauca/asae_t7/AsaeT7ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.asae_t7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsaeT7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
